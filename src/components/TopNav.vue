86847c4df15b0c68be32bc9c0bc9fb543e0e1091
Jediael Mendoza <mend0214@algonquinlive.com>
2025-12-01 17:26:53 -0500

    Final Project test

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/34-341049_best-buy-logo-png-transparent-png.png.png" alt="Best Buy Logo">
       </a>
       Admin Portal
     </div>
